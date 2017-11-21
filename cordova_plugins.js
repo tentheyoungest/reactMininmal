af9f7fbcebec7286578037fbfbab511331e26a88
jesbonzo <jessybonzo31@gmail.com>
2017-11-21 18:34:23 +0800

    reactMinimal

---
+++ cordova_plugins.js
@@ -0,0 +1,8 @@
+{
+        "id": "cordova-plugin-contacts.contacts",
+        "file": "plugins/cordova-plugin-contacts/www/contacts.js",
+        "pluginId": "cordova-plugin-contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    }
